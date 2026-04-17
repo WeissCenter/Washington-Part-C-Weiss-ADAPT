f99da01b6c5b12d67bf5bc4899c95ee1b2156a91
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-04-17 11:37:46 -0400

    initial environment updates

---
+++ apps/adapt-admin/src/environments/environment.ts
@@ -0,0 +1,29 @@
+import { NgxLoggerLevel } from 'ngx-logger';
+
+export const environment = {
+  // NgxLoggerLevels: TRACE|DEBUG|INFO|LOG|WARN|ERROR|FATAL|OFF
+  logLevel: NgxLoggerLevel.OFF,
+  API_URL: '',
+  VAPID_KEY: 'BIw-Au933HEBs8K8aTSSe15hztRM07WE-htpSG5eWnb-k2ilFGbeqlIGI7A3Xnl2lpJmaLgN-xjjsDr4oeWmLsk',
+  cognitoRegion: 'us-east-1',
+  cognitoDomainName: 'wac-prod-adaptadmin',
+  s3PublicAssetsDomainName: 'wac-prod-adaptpublicassetsbucket',
+  clientId: '',
+  contentRoot: 'assets/text',
+  contentFileName: 'admin-content-text.json',
+  appDomain: 'dcyf-wa-admin.adaptdata.org',
+  enforceLogin: true,
+  envLabel: 'Prod',
+  enforceRole: true,
+  callbackUrl: 'https://dcyf-wa-admin.adaptdata.org/auth/redirect',
+  Cognito: {
+    userPoolId: '',
+    userPoolClientId: '',
+  },
+  loginContent: 'assets/content-labels.json',
+  pagesContent: 'assets/text/admin-content-text.json',
+  organizationName: 'Department of Children, Youth, and Families (DCYF)',
+  logoPath: 'assets/shared/logos/states/wa',
+  logoExtension: 'png',
+  copyrightText: 'The State of Washington.',
+};
