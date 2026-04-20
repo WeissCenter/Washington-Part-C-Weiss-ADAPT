97ffdee0f75c33af1d90ff270fbab61f0cc3ac7b
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-04-20 09:39:07 -0400

    create state specific viewer content

--- apps/adapt-viewer/src/environments/environment.ts
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   envLabel: 'Prod',
-  contentRoot: 'assets/text/generic',
+  contentRoot: 'assets/text/wa',
   contentFileName: 'viewer-content-text.json',
   appDomain: 'dcyf-wa-viewer.adaptdata.org',
   API_URL: 'https://43admgzs06.execute-api.us-east-1.amazonaws.com/wac-prod/',
